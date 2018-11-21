cdae5936952c1514b8dd1d2e49cf1686bb366144
JoaoMarcosE <44987093+JoaoMarcosE@users.noreply.github.com>
2018-11-21 01:07:29 -0200

    Update global.js

--- src/client/js/global.js
+++ src/client/js/global.js
@@ -2,7 +2,7 @@ module.exports = {
     // Atalhos/teclas e outras constantes matemáticas
     KEY_ESC: 27,
     KEY_ENTER: 13,
-    borderDraw: false,
+    desenharBorda: false,
     spin: -Math.PI,
     ladosComida: 10,  
 
@@ -13,11 +13,11 @@ module.exports = {
     alturaJogo: 0,
     xoffset: -0,
     yoffset: -0,
-    gameStart: false,
+    jogoIniciado: false,
     desconectado: false,
     morreu: false,
     kickado: false,
-    toggleMassState: 0,
+    alternarEstadoMassa: 0,
     backgroundColor: '#f2fbff',
     lineColor: '#000000',
 };
